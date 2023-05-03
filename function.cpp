c2f56a174b4d28e1ad120fd5f3d23d8a95b3a877
KumarMangalamSingh <125680468+KumarMangalamSingh@users.noreply.github.com>
2023-05-03 04:18:54 -0700

    Create function.cpp

---
+++ function.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+int sample(int a,int b){
+	int sum=a+b;
+	return sum;
+}
+using namespace std;
+int main(){
+	
+    int a=4;
+    int b=5;
+    int sum=sample(a, b);
+    cout<<sum;
+    
+	
+	
+	
+	return 65366368;
+}
